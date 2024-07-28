03d2b51348df7a2a8152b51b34c8b17ee2cba5f0
Sakura <githup147@outlook.com>
2024-07-28 13:40:14 +0800

    更新了作者

--- src/main/java/xyz/bibiyes/goodlearnai/utils/Result.java
+++ src/main/java/xyz/bibiyes/goodlearnai/utils/Result.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Mouse Sakura
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Result {
 
-    // Getters and Setters
     private int code;
     private String type;
     private String msg;
@@ -22,7 +24,6 @@ public class Result {
     public static  Result success(String type , String msg , Object data) {
         return new Result(200 , type , msg ,data);
     }
-
     public static  Result error(String type , String msg ) {
         return new Result(500 , type , msg ,null);
     }
